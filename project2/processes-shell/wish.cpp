d16f6fc974badd8df5fc9de4119f77f437a8c8c0
jlvu <jlvu@ucdavis.edu>
2024-04-22 17:38:08 -0700

    32/80

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -2,37 +2,50 @@
 #include <string>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <cstdlib>
 #include <cstring>
+#include <fcntl.h>
+#include <cstdlib>
 
 using namespace std;
 
 const int MAX_ARGS = 256;
 const int MAX_COMMAND_LENGTH = 1024;
 
+// Function to print the shell prompt
 void print_prompt() {
     cout << "wish> ";
 }
 
+// Function to print the error message to stderr
 void print_error_message() {
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
-
+ 
+// Function to execute a command with arguments
 void execute_command(const char* command, char* const args[]) {
+    // Create a child process
     pid_t pid = fork();
     if (pid < 0) {
+        // Fork failed
         print_error_message();
     } else if (pid == 0) {
         // Child process
-        if (execvp(command, args) < 0) {
-            print_error_message();
-            exit(EXIT_FAILURE);
-        }
+        // Execute the command
+        execv(command, args);
+        // If execv returns, it indicates an error
+        print_error_message();
+        exit(EXIT_FAILURE);
     } else {
         // Parent process
+        // Wait for the child process to complete
         wait(NULL);
     }
+}
+
+// Function to check if the command is "exit"
+bool is_exit_command(const char* command) {
+    return strcmp(command, "exit") == 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -41,14 +54,13 @@ int main(int argc, char* argv[]) {
     string batch_filename;
 
     // Check if batch mode is enabled
-    if (argc > 2) {
-        print_error_message();
-        exit(EXIT_FAILURE);
-    } else if (argc == 2) {
+    if (argc > 1) {
+        // Batch mode
         interactive_mode = false;
         batch_filename = argv[1];
         input_file = fopen(batch_filename.c_str(), "r");
         if (!input_file) {
+            // Error opening batch file
             print_error_message();
             exit(EXIT_FAILURE);
         }
@@ -57,24 +69,34 @@ int main(int argc, char* argv[]) {
     // Main loop
     while (true) {
         if (interactive_mode) {
+            // Print shell prompt in interactive mode
             print_prompt();
         }
 
-        // Read command
+        // Read command from input
         char command_line[MAX_COMMAND_LENGTH];
         if (fgets(command_line, sizeof(command_line), input_file) == NULL) {
             // End of file or error
             if (interactive_mode) {
+                // Exit gracefully in interactive mode
                 cout << endl;
             }
             break;
         }
 
-        // Parse command
-        // Parse the command_line and split it into command and arguments
+        // Parse command line into command and arguments
+        // For now, let's assume the command is just one word
+        char* command = strtok(command_line, " \n");
+        if (command != NULL) {
+            // Check if the command is "exit"
+            if (is_exit_command(command)) {
+                // Exit the shell
+                exit(0);
+            }
 
-        // Execute command
-        // Call execute_command with the parsed command and arguments
+            // Execute the command
+            execute_command(command, NULL);
+        }
     }
 
     return 0;
