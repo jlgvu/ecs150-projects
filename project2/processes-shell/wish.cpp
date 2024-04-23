5b3aeecebc908803858ce957edccee9db84efca0
jlvu <jlvu@ucdavis.edu>
2024-04-23 16:55:28 -0700

    54.5/80

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,17 +6,11 @@
 #include <cstring>
 #include <fcntl.h>
 #include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
-
-const int MAX_ARGS = 256;
-const int MAX_COMMAND_LENGTH = 1024;
-const string DEFAULT_PATH = "/bin"; // Default path
-
-// Function to print the shell prompt
-void print_prompt() {
-    cout << "wish> ";
-}
 
 // Function to print the error message to stderr
 void print_error_message() {
@@ -26,10 +19,10 @@ void print_error_message() {
 }
 
 // Function to execute a command with arguments
-void execute_command(const char* command, char* const args[], const vector<string>& path_directories) {
+void execute_command(const vector<string>& command_line, const vector<string>& path_directories) {
     // Try executing the command from each directory in the path
     for (const string& directory : path_directories) {
-        string full_command = directory + "/" + command;
+        string full_command = directory + "/" + command_line[0];
         if (access(full_command.c_str(), X_OK) == 0) {
             // Command found and is executable
             pid_t pid = fork();
@@ -39,6 +32,12 @@ void execute_command(const char* command, char* const args[], const vector<strin
             } else if (pid == 0) {
                 // Child process
                 // Execute the command
+                // Convert vector<string> to array of C strings
+                char* args[command_line.size() + 1];
+                for (size_t i = 0; i < command_line.size(); ++i) {
+                    args[i] = strdup(command_line[i].c_str());
+                }
+                args[command_line.size()] = nullptr; // Null-terminate the array
                 execv(full_command.c_str(), args);
                 // If execv returns, it indicates an error
                 print_error_message();
@@ -56,55 +55,96 @@ void execute_command(const char* command, char* const args[], const vector<strin
     print_error_message();
 }
 
-// Function to handle the cd command
-void handle_cd_command(const char* directory) {
-    // Change directory using chdir
-    if (chdir(directory) != 0) {
-        // chdir failed
+// Function to parse the command line
+vector<string> parse_command_line(const string& command_line) {
+    vector<string> tokens;
+    istringstream iss(command_line);
+    string token;
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+// Function to handle the redirection
+void handle_redirection(const vector<string>& command_line) {
+    size_t pos = command_line.size();
+    for (size_t i = 0; i < command_line.size(); ++i) {
+        if (command_line[i] == ">") {
+            pos = i;
+            break;
+        }
+    }
+    if (pos == command_line.size() || pos == command_line.size() - 1) {
         print_error_message();
+        return;
     }
-}
 
-// Function to handle the path command
-void handle_path_command(char* const args[], vector<string>& path_directories) {
-    // Clear the existing path vector
-    path_directories.clear();
-
-    // Add directories from args to path_directories vector
-    int i = 1;
-    while (args[i] != NULL) {
-        path_directories.push_back(args[i]);
-        i++;
+    // Redirect stdout to the specified file
+    string filename = command_line[pos + 1];
+    int fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd == -1) {
+        print_error_message();
+        return;
     }
+    dup2(fd, STDOUT_FILENO);
+    close(fd);
+
+    // Execute the command before '>'
+    vector<string> cmd(command_line.begin(), command_line.begin() + pos);
+    execute_command(cmd, {"."}); // Use current directory as default path
 }
 
-// Function to check if the command is "exit"
-bool is_exit_command(const char* command) {
-    return strcmp(command, "exit") == 0;
-}
+// Function to handle parallel commands
+void handle_parallel_commands(const vector<string>& command_line, const vector<string>& path_directories) {
+    vector<vector<string>> commands;
+    vector<string> current_command;
+    for (const string& token : command_line) {
+        if (token == "&") {
+            commands.push_back(current_command);
+            current_command.clear();
+        } else {
+            current_command.push_back(token);
+        }
+    }
+    if (!current_command.empty()) {
+        commands.push_back(current_command);
+    }
 
-// Function to check if the command is "cd"
-bool is_cd_command(const char* command) {
-    return strcmp(command, "cd") == 0;
-}
+    vector<pid_t> pids;
+    for (const auto& cmd : commands) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            print_error_message();
+            return;
+        } else if (pid == 0) {
+            // Child process
+            execute_command(cmd, path_directories);
+            exit(EXIT_SUCCESS);
+        } else {
+            // Parent process
+            pids.push_back(pid);
+        }
+    }
 
-// Function to check if the command is "path"
-bool is_path_command(const char* command) {
-    return strcmp(command, "path") == 0;
+    // Wait for all child processes to finish
+    for (pid_t pid : pids) {
+        waitpid(pid, NULL, 0);
+    }
 }
 
 int main(int argc, char* argv[]) {
     bool interactive_mode = true;
-    FILE* input_file = stdin;
+    ifstream input_file;
     string batch_filename;
-    vector<string> path_directories = {DEFAULT_PATH}; // Initial path with DEFAULT_PATH
+    vector<string> path_directories = {"/bin", "/usr/bin"}; // Initial path
 
     // Check if batch mode is enabled
     if (argc > 1) {
         // Batch mode
         interactive_mode = false;
         batch_filename = argv[1];
-        input_file = fopen(batch_filename.c_str(), "r");
+        input_file.open(batch_filename);
         if (!input_file) {
             // Error opening batch file
             print_error_message();
@@ -116,53 +156,61 @@ int main(int argc, char* argv[]) {
     while (true) {
         if (interactive_mode) {
             // Print shell prompt in interactive mode
-            print_prompt();
+            cout << "wish> ";
         }
 
         // Read command from input
-        char command_line[MAX_COMMAND_LENGTH];
-        if (fgets(command_line, sizeof(command_line), input_file) == NULL) {
-            // End of file or error
-            if (interactive_mode) {
-                cout << endl;
+        string command_line;
+        if (interactive_mode) {
+            getline(cin, command_line);
+        } else {
+            if (!getline(input_file, command_line)) {
+                // End of file or error
+                break;
             }
-            break;
         }
 
         // Parse command line into command and arguments
-        char* command = strtok(command_line, " \n");
-        if (command != NULL) {
+        vector<string> tokens = parse_command_line(command_line);
+
+        if (!tokens.empty()) {
             // Check if the command is "exit"
-            if (is_exit_command(command)) {
-                // Check if there are additional arguments after "exit"
-                char* arg = strtok(NULL, " \n");
-                if (arg != NULL) {
-                    print_error_message();
-                } else {
-                    // Exit the shell
+            if (tokens[0] == "exit") {
+                if (tokens.size() == 1) {
                     exit(0);
-                }        
+                } else {
+                    print_error_message();
+                }
             }
             // Check if the command is "cd"
-            else if (is_cd_command(command)) {
-                // Handle the cd command
-                handle_cd_command(strtok(NULL, " \n"));
+            else if (tokens[0] == "cd") {
+                if (tokens.size() == 2) {
+                    if (chdir(tokens[1].c_str()) != 0) {
+                        print_error_message();
+                    }
+                } else {
+                    print_error_message();
+                }
             }
             // Check if the command is "path"
-            else if (is_path_command(command)) {
-                // Handle the path command
-                char* args[MAX_ARGS];
-                args[0] = command;
-                for (int i = 1; i < MAX_ARGS; i++) {
-                    args[i] = strtok(NULL, " \n");
-                    if (args[i] == NULL) break;
+            else if (tokens[0] == "path") {
+                if (tokens.size() > 1) {
+                    path_directories.assign(tokens.begin() + 1, tokens.end());
+                } else {
+                    print_error_message();
                 }
-                handle_path_command(args, path_directories);
+            }
+            // Check if the command contains '>'
+            else if (find(tokens.begin(), tokens.end(), ">") != tokens.end()) {
+                handle_redirection(tokens);
+            }
+            // Check if the command contains '&'
+            else if (find(tokens.begin(), tokens.end(), "&") != tokens.end()) {
+                handle_parallel_commands(tokens, path_directories);
             }
             // Execute other commands
             else {
-                // Execute the command
-                execute_command(command, NULL, path_directories);
+                execute_command(tokens, path_directories);
             }
         }
     }
